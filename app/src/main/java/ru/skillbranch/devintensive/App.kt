d0d4deb290e145ea3bbef468cfe6c521e44f1e81
79150 <12345ok6@mail.ru>
2020-06-04 23:28:44 +0300

    hometask_3 vers10

---
+++ app/src/main/java/ru/skillbranch/devintensive/App.kt
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Created by Yana Ostapenko, 2020
+ ******************************************************************************/
+
+package ru.skillbranch.devintensive
+
+import android.app.Application
+import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
+import ru.skillbranch.devintensive.repositories.PreferencesRepository
+
+class App: Application() {
+    companion object{
+        var instance: App? = null
+
+        fun applicationContext() : Context{
+            return instance!!.applicationContext
+        }
+    }
+
+    init {
+        instance = this
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        PreferencesRepository.getAppTheme().also {
+            AppCompatDelegate.setDefaultNightMode(it)
+        }
+        //TODO
+    }
+}
